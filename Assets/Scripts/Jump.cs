08f06d525bd4ddbe677a6aab3fb891e912689e12
choi-jonghyun <choiwhj1@naver.com>
2025-05-23 01:54:24 +0900

    feat : jump
    점프대 추가

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float launchPower = 10f;
+    public Vector3 launchDirection = Vector3.up;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody _rigidbody = other.GetComponent<Rigidbody>();
+        if(_rigidbody != null)
+        {
+            _rigidbody.velocity = Vector3.zero;
+
+            _rigidbody.AddForce(launchDirection.normalized *  launchPower, ForceMode.Impulse);
+        }
+    }
+}
