e6089858c461edcb4c752787e113f8d105bedc0b
choi-jonghyun <choiwhj1@naver.com>
2025-05-23 12:32:21 +0900

    feat : jump 주석
    주석처리

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    public float launchPower = 10f;
-    public Vector3 launchDirection = Vector3.up;
+    public float launchPower = 10f; //점프 힘
+    public Vector3 launchDirection = Vector3.up;    //점프방향
 
+    //트리거실행
     private void OnTriggerEnter(Collider other)
     {
+        //리지드바디 가져옴
         Rigidbody _rigidbody = other.GetComponent<Rigidbody>();
+        //리지드바디가 있으면
         if(_rigidbody != null)
         {
+            //속도를 0으로
             _rigidbody.velocity = Vector3.zero;
 
+            //설정한 방향으로 점프함
             _rigidbody.AddForce(launchDirection.normalized *  launchPower, ForceMode.Impulse);
         }
     }
