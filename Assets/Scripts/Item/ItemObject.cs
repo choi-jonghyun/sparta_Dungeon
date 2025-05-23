f34c6dc25a96d0893ae789720944315232298c92
choi-jonghyun <choiwhj1@naver.com>
2025-05-23 12:08:15 +0900

    feat : itemobject 주석
    주석

--- Assets/Scripts/Item/ItemObject.cs
+++ Assets/Scripts/Item/ItemObject.cs
@@ -2,24 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//상호작용 인터페이스
 public interface IInteractable
 {
-    public string GetInteractPrompt();
-    public void OnInteract();
+    public string GetInteractPrompt();      //상호작용 시 화면에 표시 
+    public void OnInteract();   //상호작용 
 }
 
 public class ItemObject : MonoBehaviour, IInteractable
 {
     public ItemData data;
 
-    public string GetInteractPrompt()
+    public string GetInteractPrompt()   //상호작용 할 때 표시 문구
     {
-        string str = $"{data.displayName}\n{data.description}";
+        string str = $"{data.displayName}\n{data.description}"; //이름과 설명을 줄바꿈하고 표시
         return str;
     }
-    public void OnInteract()
+    public void OnInteract() //상호작용시 실행
     {
+        // 플레이어의 아이템데이터에 아이템데이터 전달
         CharacterManager.Instance.Player.itemData = data;
+        //플레이어의 아이템추가 호출
         CharacterManager.Instance.Player.addItem?.Invoke();
         Destroy(gameObject);
     }
