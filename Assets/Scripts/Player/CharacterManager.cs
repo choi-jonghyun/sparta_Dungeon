55f72977dc4f343d6a0dbbf1576888265569593f
choi-jonghyun <choiwhj1@naver.com>
2025-05-23 12:08:36 +0900

    feat : CharacterManager주석
    주석처리

--- Assets/Scripts/Player/CharacterManager.cs
+++ Assets/Scripts/Player/CharacterManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
+    //싱글톤
     private static CharacterManager _instance;
+
+    
     public static CharacterManager Instance
     {
         get
         {
+            //인스턴스가 없으면 새 게임오브젝트 캐릭터매니저를 추가
             if(_instance == null)
             {
                 _instance = new GameObject("CharacterManager").AddComponent<CharacterManager>();
@@ -16,9 +20,9 @@ public class CharacterManager : MonoBehaviour
             return _instance;
         }
     }
-    public Player _player;
+    public Player _player;  //플레이어 정보를 저장
 
-    public Player Player
+    public Player Player        //플레이어 접근용
     {
         get {  return _player; }
         set { _player = value; }
@@ -26,6 +30,7 @@ public class CharacterManager : MonoBehaviour
 
     private void Awake()
     {
+        //인스턴스가 없으면 인스턴스로 설정하고 씬변경시 파괴되지않음
         if(_instance == null)
         {
             _instance = this;
@@ -33,6 +38,7 @@ public class CharacterManager : MonoBehaviour
         }
         else
         {
+            //인스턴스가 있다면 파괴
             if(_instance != this)
             {
                 Destroy(gameObject);
