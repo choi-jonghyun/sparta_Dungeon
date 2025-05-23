44411b98491257fc20c2a94d379e728d0466f295
choi-jonghyun <choiwhj1@naver.com>
2025-05-23 12:10:27 +0900

    feat : uicondition 주석
    주석처리

--- Assets/Scripts/UI/UICondition.cs
+++ Assets/Scripts/UI/UICondition.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class UICondition : MonoBehaviour
 {
-    public Condition health;
-    public Condition hurger;
-    public Condition stamina;
+    public Condition health;    //체력
+    public Condition hunger;    //배고픔
+    public Condition stamina;   //스태미나
 
     // Start is called before the first frame update
     private void Start()
     {
+        //플레이어컨디션에서 UI컨디션을 연결
         CharacterManager.Instance.Player.condition.uiCondition = this;
     }
 
