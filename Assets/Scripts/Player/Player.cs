479162068c6eacccef3576cb0fe34cc1674dd169
choi-jonghyun <choiwhj1@naver.com>
2025-05-23 12:09:10 +0900

    feat : player 주석
    주석처리

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public PlayerController controller;
-    public PlayerCondition condition;
-    public ItemData itemData;
-    public Action addItem;
+    public PlayerController controller;     //플레이어컨트롤
+    public PlayerCondition condition;       //플레이어상태
+    public ItemData itemData;               //보유한아이템데이터
+    public Action addItem;                  //아이템추가시
 
-    public Transform dropPosition;
+    public Transform dropPosition;          //아이템 드랍위치
 
     private void Awake()
     {
+        //캐릭터매니저 싱클톤에 플레이어 인스턴스등록
         CharacterManager.Instance.Player = this;
+        //플레이어컨트롤러 컨디션 컴포넌트 가져옴
         controller = GetComponent<PlayerController>();
         condition = GetComponent<PlayerCondition>();
     }
